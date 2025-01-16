8480d12e18ea24ae9fd6d0afcfc378d57276ef6b
Krzysztof Stankiewicz <stona013@hotmail.de>
2025-01-17 00:10:08 +0100

    load header

--- js/loadHeaderFooter.js
+++ js/loadHeaderFooter.js
@@ -1,15 +1,22 @@
-// loadHeaderFooter.js
-
+// Funktion zum Laden einer HTML-Datei in ein bestimmtes Element
 function loadHTML(file, elementId) {
     fetch(file)
-      .then(response => response.text())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Netzwerkfehler beim Laden der Datei');
+        }
+        return response.text();
+      })
       .then(data => {
         document.getElementById(elementId).innerHTML = data;
       })
-      .catch(error => console.error('Error loading HTML:', error));
+      .catch(error => {
+        console.error('Fehler beim Laden der Datei:', error);
+      });
   }
   
-  window.onload = () => {
+  // Laden des Headers und Footers nach dem Laden der Seite
+  window.onload = function () {
     loadHTML('partials/header.html', 'header-container');
     loadHTML('partials/footer.html', 'footer-container');
   };
